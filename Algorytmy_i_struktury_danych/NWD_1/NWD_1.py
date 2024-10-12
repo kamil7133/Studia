f404859999b0bf8f6feba9851690ff322e91010d
kamil7133 <piwowarczykkamil23@gmail.com>
2024-10-12 11:38:27 +0200

    initial commit

--- Algorytmy_i_struktury_danych/NWD_1/NWD_1.py
+++ Algorytmy_i_struktury_danych/NWD_1/NWD_1.py
@@ -1,1 +1,23 @@
-class zadanie:
+#algorytm euklidesa
+
+class Zadanie:
+    def nwd(self, a, b): #funkcja z inputami a i b
+        if a and b > 0: #jezeli a i b jest wieksze od 0 to wykonuj
+            while a != b: #dopóki a nie jest równe b
+                if a > b: #jezeli a wieksze od b
+                    a -= b #to odejmij b od a
+                else:
+                    b -= a #odejmij a od b
+            return a
+        else: #w innym wypadku wydrukuj
+            print("Złe liczby")
+
+a, b = input("Podaj dwie liczby: ").split() #.split dzieli podana wartosc dwoch liczb odzielona spacja na dwie zmienne
+a = int(a) #deklaracja ze a jest liczba calkowita
+b = int(b) #-=-
+zadanie = Zadanie()
+wynik = zadanie.nwd(a, b)
+if a and b > 0: #jeszcze raz sprawdzenie czy a i b jest mniejsze od 0, jezeli nie to uzytkownik podal zle liczby
+    print(f"Najwiekszy wspolny dzielnik to: {wynik}")
+
+#liczby podawać bez przecinka np. 892 292
---
+++ Algorytmy_i_struktury_danych/NWD_2/NWD_2.py
@@ -0,0 +1,15 @@
+class Zadanie:
+    def nwd(self, a, b):
+        while a % b != 0: #dopoki reszta z dzielenia a i b nie jest rowna 0
+            c = a % b #przypisanie reszty z dzielenia a i b do zmiennej pomocniczej c
+            a = b
+            b = c
+        return b
+
+zadanie = Zadanie()
+a, b = input("Podaj dwie liczby: ").split() #.split dzieli podana wartosc dwoch liczb odzielona spacja na dwie zmienne
+a = int(a) #deklaracja ze a jest liczba calkowita
+b = int(b)
+wynik = zadanie.nwd(a, b)
+print(f"Reszta z dzielenia to {wynik}") #wyswietlenie wyniku
+
