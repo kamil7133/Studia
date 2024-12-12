08cd566d0d24908e37f9a93f7658095b1597f0d2
kamil7133 <piwowarczykkamil23@gmail.com>
2024-12-12 13:23:28 +0100

    initial commit

--- ProgramowanieLab/lista2dodatkowe.py
+++ ProgramowanieLab/lista2dodatkowe.py
@@ -6,11 +6,11 @@ elif i > 0:
     print("liczba jest dodatnia")
 else:
     print("liczba jest rowna zero")
-zad 2
+#zad 2
 pkt = int(input("Podaj liczbe punktow od 0 do 100: "))
 if pkt < 0 or pkt > 100:
     print("liczba jest nieprawidlowa")
-elif pkt > 0 and pkt < 50:
+elif pkt >= 0 and pkt <= 50:
     print("Ocena 2.0")
 elif pkt > 51 and pkt < 60:
     print("Ocena 3.0")
@@ -22,9 +22,7 @@ elif pkt > 81 and pkt < 90:
     print("Ocena 4.5")
 elif pkt > 91 and pkt < 100:
     print("Ocena 5.0")
-else:
-    print("Nieprawidlowy ciag znakow")
-zad 3
+#zad 3
 password = "maslo"
 pas_uzy = input("Podaj haslo: ")
 if password == pas_uzy:
@@ -42,9 +40,9 @@ if dlugosc > zakres:
 else:
     print("Poprawne !")
 # zad 5
-a = input("Podaj liczbe a: ")
-b = input("Podaj liczbe b: ")
-c = input("Podaj liczbe c: ")
+a = int(input("Podaj liczbe a: "))
+b = int(input("Podaj liczbe b: "))
+c = int(input("Podaj liczbe c: "))
 if a < 0 or b < 0 or c < 0:
     print("Niepoprawne !")
 elif a**2 + b**2 == c**2:
@@ -53,17 +51,20 @@ else:
     print("Nie spelniaja warunku pitagorasa")
 
 # zad 6
-q = input("Podaj q: ")
-p = input("Podaj p: ")
-if not(p and q) == (not(p) or not(q)):
+q = bool(input("Podaj q: "))
+p = bool(input("Podaj p: "))
+if not (p and q) == (not p or not q):
     print("Sa takie same")
 else:
-    print("Nie sa takie same")
+    print("cos poszlo nie tak")
 # zad 7
 liczba_dni = int(input("Podaj liczbe dni: "))
 if liczba_dni >= 10:
-    print("150zl")
+    cena1 = liczba_dni * 150
+    print(f"cena za hotel wyniesie: {cena1}")
 elif liczba_dni < 10 and liczba_dni > 5:
-    print("200zl")
-elif liczba_dni < 5:
-    print("250zl")
+    cena2 = liczba_dni * 200
+    print(f"cena za hotel wyniesie: {cena2}")
+elif liczba_dni <= 5:
+    cena3 = liczba_dni * 250
+    print(f"cena za hotel wyniesie: {cena3}")
